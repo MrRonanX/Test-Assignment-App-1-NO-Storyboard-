a7823a7ad6371d72ea3786cb34b5ca5d010f2f23
MrRonanX <57289601+MrRonanX@users.noreply.github.com>
2020-06-16 16:32:09 +0700

    Initial Commit

---
+++ testAssignment App 1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testAssignment App 1
+//
+//  Created by Roman Kavinskyi on 16.06.2020.
+//  Copyright © 2020 Roman Kavinskyi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
